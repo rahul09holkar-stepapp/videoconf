37df7ce3393287a126847da640f5541af097fe71
rahul09holkar-stepapp <root@ip-192-168-0-89.ap-south-1.compute.internal>
2020-06-09 07:14:33 +0000

    by auto deployer

--- public/client.js
+++ public/client.js
@@ -64,7 +64,6 @@ socket.on('created', function (room,SocketId) {
     }).catch(function (err) {
         console.log('An error ocurred when accessing media devices', err);
     });
-	console.log(localVideoS);
 });
 
 socket.on('joined', function (room,SocketId) {
